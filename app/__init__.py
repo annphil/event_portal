3be7f5de1d4dcce0a905d1e74eb4c9017d8bd2c1
atp <hannathoppil>
2021-05-25 14:31:57 +0530

    micro to eventza

--- app/__init__.py
+++ app/__init__.py
@@ -40,7 +40,7 @@ def create_app(config_class=Config):
         else:
             if not os.path.exists('logs'):
                 os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/microblog.log',
+            file_handler = RotatingFileHandler('logs/eventza.log',
                                                maxBytes=10240, backupCount=10)
             file_handler.setFormatter(logging.Formatter(
                 '%(asctime)s %(levelname)s: %(message)s '
@@ -49,7 +49,7 @@ def create_app(config_class=Config):
             app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
-        app.logger.info('Microblog startup')
+        app.logger.info('Eventza startup')
 
     return app
 
